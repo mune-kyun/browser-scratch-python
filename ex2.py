0bd0218fe199ebb51e265e519d9c6a175cc6c450
mune-kyun <rzknurfaizi@gmail.com>
2024-08-17 20:42:15 +0700

    resizing browser

--- ex2.py
+++ ex2.py
@@ -1,18 +1,22 @@
 import tkinter
 from ex1 import URL, lex
 
-HEIGHT, WIDTH = 600, 800
-HSTEP, VSTEP = 13, 18
-SCROLL_STEP = 100
 class Browser:
+    HEIGHT, WIDTH = 600, 800
+    HSTEP, VSTEP = 13, 18
+    SCROLL_STEP = 100
+    
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
-            width=WIDTH,
-            height=HEIGHT
+            width=self.WIDTH,
+            height=self.HEIGHT
         )
-        self.canvas.pack()
+        self.canvas.pack(
+            fill=tkinter.BOTH,
+            expand=1
+        )
         self.scroll_val = 0
         self.display_list = []
         self.bind_keys()
@@ -20,7 +24,17 @@ class Browser:
     def bind_keys(self):
         self.window.bind("<Down>", lambda e: self.scroll("<Down>"))
         self.window.bind("<Up>", lambda e: self.scroll("<Up>"))
+        self.window.bind("<Configure>", lambda e: self.handle_configure(e))
         self.canvas.bind("<MouseWheel>", self.handle_mouse_wheel)
+
+    def handle_configure(self, e):
+        width, height = e.width, e.height
+        if abs(self.WIDTH - width) > 9 or abs(self.HEIGHT - height) > 9:
+            self.WIDTH = width
+            self.HEIGHT = height
+            self.display_list = self.layout(self.text)
+            self.canvas.delete("all")
+            self.draw()
 
     def scroll(self, direction):
         if len(self.display_list) == 0:
@@ -31,7 +45,7 @@ class Browser:
             _, y, _ = char
             
             if self.y_below_screen(y):
-                self.scroll_val += SCROLL_STEP
+                self.scroll_val += self.SCROLL_STEP
             else:
                 return
             
@@ -40,7 +54,7 @@ class Browser:
             _, y, _ = char
 
             if self.y_above_screen(y):
-                self.scroll_val -= SCROLL_STEP
+                self.scroll_val -= self.SCROLL_STEP
             else:
                 return
 
@@ -55,9 +69,9 @@ class Browser:
 
     def load(self, url: URL):
         res = url.request()
-        text = lex(res["content"])
+        self.text = lex(res["content"])
 
-        self.display_list = layout(text)
+        self.display_list = self.layout(self.text)
         self.draw()
 
     def y_above_screen(self, y):
@@ -66,7 +80,7 @@ class Browser:
     
     def y_below_screen(self, y):
         y_dest = y - self.scroll_val
-        return y_dest > HEIGHT
+        return y_dest > self.HEIGHT
 
     def draw(self):
         for x, y, c in self.display_list:
@@ -74,22 +88,22 @@ class Browser:
             if self.y_above_screen(y) or self.y_below_screen(y): continue
             self.canvas.create_text(x, y_dest, text=c)
 
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
+    def layout(self, text):
+        display_list = []
+        cursor_x, cursor_y = self.HSTEP, self.VSTEP
+        for c in text:
+            display_list.append((cursor_x, cursor_y, c))
+            cursor_x += self.HSTEP
 
-        if "\n" in c:
-            cursor_x = HSTEP
-            cursor_y += 1.2*VSTEP
-        
-        if cursor_x >= WIDTH + HSTEP:
-            cursor_x = HSTEP
-            cursor_y += VSTEP
+            if "\n" in c:
+                cursor_x = self.HSTEP
+                cursor_y += 1.2*self.VSTEP
+            
+            if cursor_x >= self.WIDTH + self.HSTEP:
+                cursor_x = self.HSTEP
+                cursor_y += self.VSTEP
 
-    return display_list
+        return display_list
 
 if __name__ == "__main__":
     import sys
